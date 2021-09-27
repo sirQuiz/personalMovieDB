3596efd13e1ce5b5f0a72f0c27dbf81e548c9df4
Sergey Kovalchuk <sergey.kovalchuk.utd@gmail.com>
2021-09-27 23:10:50 +0300

    added functions for past loops. Added new functions to check private and write genres.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 (function(){
-    const nubmerOfFilms = prompt("How many films did you watch?", "0");
-    let    lastSeenMovie, rateMovie;    
+    let nubmerOfFilms, lastSeenMovie, rateMovie;
+    
+    function start() {
+        while(nubmerOfFilms == '' || nubmerOfFilms == null || isNaN(nubmerOfFilms)) {
+            nubmerOfFilms = +prompt("How many films did you watch?", "");
+        }
+    }
+    start();
+
     const personalMovieDB = {
         count: nubmerOfFilms,
         movies: {},
@@ -9,25 +16,48 @@
         privat: false,
     };
 
-    for(let i = 0; i < 2; i++) {
+    function userMoviesAndRates(lastMovies) {
+        for(let i = 0; i < 2; i++) {
             lastSeenMovie = prompt("What was the last movie you saw?", "None");
-            rateMovie     = prompt("Rate last movie you saw.", "");  
-        if(lastSeenMovie == "" || rateMovie == "" || lastSeenMovie == null || rateMovie == null || lastSeenMovie.length > 50) {
-            i--;    
-        } else {   
-            personalMovieDB.movies[lastSeenMovie] = rateMovie;
+            rateMovie     = +prompt("Rate last movie you saw.", ""); 
+
+            if(lastSeenMovie == "" || rateMovie == "" || lastSeenMovie == null || rateMovie == null || lastSeenMovie.length > 50 || isNaN(rateMovie)) {
+                i--;    
+            } else {   
+                lastMovies[lastSeenMovie] = rateMovie;
+            }
         }
     }
+    userMoviesAndRates(personalMovieDB.movies);
 
-    if(personalMovieDB.count < 10) {
-        alert("You watched just a few movies.");
-    } else if(10 <= personalMovieDB.count && personalMovieDB.count < 30) {
-        alert("You are ordinary viewer.");
-    } else if(personalMovieDB.count >= 30) {
-        alert("You are movie fan.");
-    } else {
-        alert("Error.");
+    function userMovieStatus(count) {
+        if(count < 10) {
+            alert("You watched just a few movies.");
+        } else if(10 <= count && count < 30) {
+            alert("You are ordinary viewer.");
+        } else if(count >= 30) {
+            alert("You are movie fan.");
+        } else {
+            alert("Error.");
+        }
     }
+    userMovieStatus(personalMovieDB.count);
 
-    console.log(personalMovieDB.movies);
+    function showUserDB(hidden) {
+        if(!hidden) {
+            console.log(personalMovieDB);
+        } else {
+            console.log("Movie DataBase is privat.");
+        }
+    }
+    showUserDB(personalMovieDB.privat);
+
+    function writeYourGenres(genres) {
+        for(let i = 0; i < 3; i++) {
+            const userGenre = prompt(`You favorite genre on ${i+1} place`, "");
+            genres[i] = userGenre;
+        }
+    }
+    writeYourGenres(personalMovieDB.genres);
+
 }());
