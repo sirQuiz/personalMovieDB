27bd0440e78784dbddcb5769182813e9f3f0ddc0
Sergey Kovalchuk <sergey.kovalchuk.utd@gmail.com>
2021-10-07 19:03:12 +0300

    refactored code - redone all function like a methods of object

--- scripts.js
+++ scripts.js
@@ -14,50 +14,47 @@
         actors: {},
         genres: [],
         privat: false,
-    };
-
-    function userMoviesAndRates(lastMovies) {
-        for(let i = 0; i < 2; i++) {
-            lastSeenMovie = prompt("What was the last movie you saw?", "None");
-            rateMovie     = +prompt("Rate last movie you saw.", ""); 
-
-            if(lastSeenMovie == "" || rateMovie == "" || lastSeenMovie == null || rateMovie == null || lastSeenMovie.length > 50 || isNaN(rateMovie)) {
-                i--;    
-            } else {   
-                lastMovies[lastSeenMovie] = rateMovie;
+        userMoviesAndRates: function(lastMovies) {
+            for(let i = 0; i < 2; i++) {
+                lastSeenMovie = prompt("What was the last movie you saw?", "None");
+                rateMovie     = +prompt("Rate last movie you saw.", ""); 
+    
+                if(lastSeenMovie == "" || rateMovie == "" || lastSeenMovie == null || rateMovie == null || lastSeenMovie.length > 50 || isNaN(rateMovie)) {
+                    i--;    
+                } else {   
+                    lastMovies[lastSeenMovie] = rateMovie;
+                }
+            }
+        },
+        userMovieStatus: function(count) {
+            if(count < 10) {
+                alert("You watched just a few movies.");
+            } else if(10 <= count && count < 30) {
+                alert("You are ordinary viewer.");
+            } else if(count >= 30) {
+                alert("You are movie fan.");
+            } else {
+                alert("Error.");
+            }
+        },
+        showUserDB: function(hidden) {
+            if(!hidden) {
+                console.log(personalMovieDB);
+            } else {
+                console.log("Movie DataBase is privat.");
+            }
+        },
+        writeYourGenres: function(genres) {
+            for(let i = 0; i < 3; i++) {
+                const userGenre = prompt(`You favorite genre on ${i+1} place`, "");
+                genres[i] = userGenre;
             }
         }
-    }
-    userMoviesAndRates(personalMovieDB.movies);
+    };
 
-    function userMovieStatus(count) {
-        if(count < 10) {
-            alert("You watched just a few movies.");
-        } else if(10 <= count && count < 30) {
-            alert("You are ordinary viewer.");
-        } else if(count >= 30) {
-            alert("You are movie fan.");
-        } else {
-            alert("Error.");
-        }
-    }
-    userMovieStatus(personalMovieDB.count);
-
-    function showUserDB(hidden) {
-        if(!hidden) {
-            console.log(personalMovieDB);
-        } else {
-            console.log("Movie DataBase is privat.");
-        }
-    }
-    showUserDB(personalMovieDB.privat);
-
-    function writeYourGenres(genres) {
-        for(let i = 0; i < 3; i++) {
-            const userGenre = prompt(`You favorite genre on ${i+1} place`, "");
-            genres[i] = userGenre;
-        }
-    }
-    writeYourGenres(personalMovieDB.genres);
+    personalMovieDB.userMoviesAndRates(personalMovieDB.movies);
+    personalMovieDB.userMovieStatus(personalMovieDB.count);
+    personalMovieDB.showUserDB(personalMovieDB.privat);
+    personalMovieDB.writeYourGenres(personalMovieDB.genres);
 
 }());
